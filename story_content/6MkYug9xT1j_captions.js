c530e36d6e62861290d99d78c0527f53d8d10a52
ahaddad72 <alexa.haddad72@gmail.com>
2025-12-31 12:11:10 -1000

    Add

---
+++ story_content/6MkYug9xT1j_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6MkYug9xT1j_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.103.35301.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:01.643%0D%0AThis%20slide%20has%20no%20audio.%0D%0A%0D%0A"}]})
